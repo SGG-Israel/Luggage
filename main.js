f2e9dea0f3796e270c26acc19d8f3a2131cd02a0
Evgeni Schmidt <djonschmidt@gmail.com>
2023-01-07 20:18:53 +0200

    JS delete comint

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-let offset = 0; // смещение от левого края
+let offset = 0;
 const sliderLine = document.querySelector(".slider-line");
 
 document.querySelector(".left-next").addEventListener("click", function () {
-  //Тут мы находим нашу кнопку Вправо и слушаем её на действие Click //
-  offset = offset + 155; // Запись можно сократить и написать  offset += 200;
+  offset = offset + 155;
   if (offset > 1050) {
     offset = 0;
   }
-  sliderLine.style.left = -offset + "px"; // Сдвигаем нашу линию с блоками на ширину offset
+  sliderLine.style.left = -offset + "px";
 });
 document.querySelector(".right-next").addEventListener("click", function () {
   offset = offset - 155;
