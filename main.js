30cdca538ee3756e5244431f9506ea5257dd652c
Evgeni Schmidt <djonschmidt@gmail.com>
2023-01-07 19:18:21 +0200

    Add JS fail

---
+++ main.js
@@ -0,0 +1,18 @@
+let offset = 0; // смещение от левого края
+const sliderLine = document.querySelector(".slider-line");
+
+document.querySelector(".left-next").addEventListener("click", function () {
+  //Тут мы находим нашу кнопку Вправо и слушаем её на действие Click //
+  offset = offset + 150; // Запись можно сократить и написать  offset += 200;
+  if (offset > 1050) {
+    offset = 0;
+  }
+  sliderLine.style.left = -offset + "px"; // Сдвигаем нашу линию с блоками на ширину offset
+});
+document.querySelector(".right-next").addEventListener("click", function () {
+  offset = offset - 150;
+  if (offset < 0) {
+    offset = 1050;
+  }
+  sliderLine.style.left = -offset + "px";
+});
